00d339597fd3f6bb205e2d50392e77847f56af74
Fuad Hasan <fuad.cs22@gmail.com>
2023-11-11 00:01:02 +0600

    config

--- build/libs/database.js
+++ build/libs/database.js
@@ -8,11 +8,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 import { createClient } from 'redis';
+//use dotenv to get environment variables
+import dotenv from 'dotenv';
+dotenv.config();
+const { host, password, port } = process.env;
 export const redis = createClient({
-    password: 'vRFKwyYjKAyT3JlHjOvRj1tzGkFKYsnR',
+    password: password,
     socket: {
-        host: 'redis-10025.c282.east-us-mz.azure.cloud.redislabs.com',
-        port: 10025
+        host: host,
+        port: Number(port)
     }
 });
 redis.connect();
@@ -21,68 +25,6 @@ redis.flushAll();
 try {
     redis.on('connect', () => __awaiter(void 0, void 0, void 0, function* () {
         console.log('Redis Connected');
-        /*
-        redis.json.set("chat:12345", ".", {
-            "users": {
-                "123-456": {
-                    "name": "Alex",
-                    "avatar": "mew",
-                    "uid": "abc-def",
-                    "socketId": "socket-123",
-                    "joined": 1626240000000
-                },
-                "123-457": {
-                    "name": "Kyra",
-                    "avatar": "pikachu",
-                    "uid": "acd-def",
-                    "socketId": "socket-124",
-                    "joined": 1626240000000
-                },
-                "123-458": {
-                    "name": "Kai",
-                    "avatar": "bulbasaur",
-                    "uid": "abe-def",
-                    "socketId": "socket-125",
-                    "joined": 1626240000000
-                }
-            },
-            "activeUsers": 1,
-            "maxUsers": 2,
-            "admin": "1",
-            "created": 1626240000000,
-            "keyName": "12345"
-        });
-
-        redis.json.set("chat:12346", ".", {
-            "users": {
-                "234-456": {
-                    "name": "Wayne",
-                    "avatar": "raichu",
-                    "uid": "sbc-def",
-                    "socketId": "socket-001",
-                    "joined": 1626240000000
-                },
-                "234-457": {
-                    "name": "Hannah",
-                    "avatar": "charmander",
-                    "uid": "sbd-pqf",
-                    "socketId": "socket-002",
-                    "joined": 1626240000000
-                }
-            },
-            "activeUsers": 1,
-            "maxUsers": 2,
-            "admin": "1",
-            "created": 1626240000000,
-            "keyName": "12346"
-        });
-
-        const {activeUsers, maxUsers, users} = await redis.json.get("chat:12345", {
-            path: ["activeUsers", "maxUsers", "users"]
-          }) as Key;
-          
-        console.log(activeUsers, maxUsers, users);
-        */
     }));
     redis.on('error', (error) => {
         console.log('Redis Error', error);
