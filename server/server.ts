d205c438e8b03f29acee7d55f0970f7a443444da
Fuad Hasan <fuad.cs22@gmail.com>
2023-11-13 12:24:54 +0600

    ..

--- server/server.ts
+++ server/server.ts
@@ -6,14 +6,14 @@ const port = 3000;
 
 
 //handle requests
-httpServer.on('request', (_, response) => {
-    //console.log('Request Received');
-    
-    const {password, host, port} = Deno.env.toObject();
-    
-    console.log('Cred: ', password, host, port);
-    response.write(`Cred: ${password}, ${host}, ${port}`);
-    response.end();
+httpServer.on('request', (req, response) => {
+    if (req.url === '/') {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(`<h1>Server Running</h1>`);
+    } else if (req.url === '/ping'){
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end('pong');
+    }
 });
 
 httpServer.listen(port, () => {
