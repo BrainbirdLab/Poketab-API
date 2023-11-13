52fa9b851baf6c3c2a84258705e75bdbb2a88f20
Fuad Hasan <fuad.cs22@gmail.com>
2023-11-13 12:13:33 +0600

    ..

--- server/libs/database.ts
+++ server/libs/database.ts
@@ -3,7 +3,7 @@ import 'https://deno.land/x/dotenv/load.ts';
 
 const {password, host, port} = Deno.env.toObject();
 
-console.log(password, host, port);
+console.log('Cred: ', password, host, port);
 
 export const redis = createClient({
     password: password,
